32e90fb8df1169af453e0862923e4574a0e16e7c
Oliver Steele <steele@osteele.com>
2008-04-02 13:37:31 -0400

    remove REST library and generators
    This functionality is now built into Rails

--- generators/rest_controller/templates/controller.rb
+++
@@ -1,9 +0,0 @@
-class <%= class_name %>Controller < ApplicationController
-  # The following line defines methods that implement the OpenLaszlo
-  # REST API.
-  #
-  # To replace this file with a file that contains explicit method
-  # definitions, execute:
-  #   script/generate rest_scaffold <%= singular_name %>
-  rest_scaffold :<%= singular_name %>
-end
