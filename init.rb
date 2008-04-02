c761a5ee5341c625f3bbb78b02e4d02450f22e9d
Oliver Steele <steele@osteele.com>
2008-04-02 13:44:44 -0400

    plugin init installs javascript

--- init.rb
+++ init.rb
@@ -1,1 +1,7 @@
 require 'flashobject_view_helper'
+
+begin
+  require File.dirname(__FILE__) + '/install'
+rescue
+  raise $! unless RAILS_ENV == 'production'
+end
---
+++ install.rb
@@ -0,0 +1,3 @@
+require "#{File.dirname(__FILE__)}/lib/openlaszlo_installer"
+
+OpenLaszlo::Rails.install_javascripts
